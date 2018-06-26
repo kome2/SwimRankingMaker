87f833d2abdcebd778e86269ec328c8fabdf0b17
kcabo <k7cabo@gmail.com>
2018-06-26 16:14:40 +0900

    数ヶ月前に作ったやつ

--- swimrecord_get_html/Program.cs
+++ swimrecord_get_html/Program.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace swimrecord_get_html
+namespace get_HTML
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("swimrecord");
+
+            WebClient hanfuri = new WebClient();
+            string str = hanfuri.DownloadString("http://www.swim-record.com/ranking/ranking_17/--/112100.html");
+
+
+            StreamWriter writer = new StreamWriter(@"C:\Users\k7cab\Documents\test\test.txt");
+            writer.WriteLine(str);
+            writer.Close();
+
+            Process p = Process.Start("C:\\Users\\k7cab\\Documents\\test\\test.txt");
+
         }
     }
 }
