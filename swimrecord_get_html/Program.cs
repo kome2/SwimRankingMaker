57aa74beed1b0068e35a5ba7440939162df56e67
kcabo <k7cabo@gmail.com>
2018-06-26 16:08:39 +0900

    スイムレコードどっとこむのランキングをhtmlで抽出する

---
+++ swimrecord_get_html/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swimrecord_get_html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("swimrecord");
+        }
+    }
+}
