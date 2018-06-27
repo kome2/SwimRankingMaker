8c77604f4ca9e7e45c065ce5d3e3c3c96bf3a87d
Genta Imai <kome@hongo.wide.ad.jp>
2018-06-27 22:34:58 +0900

    コメントアウトの使い方をマスターしよう

--- swimrecord_get_html/Program.cs
+++ swimrecord_get_html/Program.cs
@@ -13,16 +13,23 @@ namespace get_HTML
     {
         static void Main(string[] args)
         {
+            // 全体的に固定文字列は変数に格納しましょう
+            // まずは日本語でいいので、コメントアウトしながら処理を記述し、
+            // 一つずつコードに起こしていくと最初は楽です
 
             WebClient hanfuri = new WebClient();
-            string str = hanfuri.DownloadString("http://www.swim-record.com/ranking/ranking_17/--/112100.html");
+            // string str = hanfuri.DownloadString("http://www.swim-record.com/ranking/ranking_17/--/112100.html");
+            string dataUrl = "http://www.swim-record.com/ranking/ranking_17/--/112100.html";
+            string str = hanfuri.DownloadString(dataUrl);
 
-
-            StreamWriter writer = new StreamWriter(@"C:\Users\k7cab\Documents\test\test.txt");
+            // StreamWriter writer = new StreamWriter(@"C:\Users\k7cab\Documents\test\test.txt");
+            string outouPath = @"C:\swim\test.txt";
+            StreamWriter writer = new StreamWriter(outouPath);
             writer.WriteLine(str);
             writer.Close();
 
-            Process p = Process.Start("C:\\Users\\k7cab\\Documents\\test\\test.txt");
+            // Process p = Process.Start("C:\\Users\\k7cab\\Documents\\test\\test.txt");
+            Process p = Process.Start(outouPath);
 
         }
     }
